3cec860c846302695b104adcce9b13d64a43d5bc
Andrieli <andrieliluci@gmail.com>
2025-05-29 18:18:07 -0300

    corrigir região escalar

--- vetorial/cod_teste2.asm
+++ vetorial/cod_teste2.asm
@@ -4,25 +4,7 @@ v.sub r3, r3
 v.add r2, r0
 v.add r3, r0
 
-sub r1, r1
-sub r2, r2
-movl 3
-add r2, r1
-
-movh 0x1
-movl 0x6
-brzr r2, r1
-
-v.st r3, r2
-v.sub r1, r1
-v.movl 4
-v.add r2, r1
-v.add r3, r1
-
-sub r1, r1
-movl 1
-sub r2, r1
-
-movh 0x0
-movl 0x9
-brzr r0, r1
+s.sub r1, r1
+s.sub r2, r2
+s.movl 3
+s.add r2, r1
